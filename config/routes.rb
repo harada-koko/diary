9da3ec0569b64283eaa2ef906ce8ff345c953205
harada-koko <harada_k@tokium.jp>
2025-06-02 14:04:16 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"  # トップページをpagesコントローラーのhomeアクションに設定
+  get "/about", to: "pages#about", as: :about # /about で pages#about に、about_path ヘルパーも作成
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # (中略) 他のルーティング ...
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
